48a91194a390639bb92d82c228c082d5bb234cc3
Jesús García <jesusgarciaamaro@gmail.com>
2025-08-23 22:10:08 -0600

    Escape key event was added to modal galerry

--- src/modal-gallery/modal-gallery.js
+++ src/modal-gallery/modal-gallery.js
@@ -3,11 +3,14 @@ function createModalGallery(options) {
     console.error('Element ID is required to create the modal gallery.');
     return;
   }
+
   var galleryContainer = renderModalGallery(options.idElement);
   var gridGalleryContainer = galleryContainer.querySelector(".gallery-carousel-grid");
   var bulletsContainer = galleryContainer.querySelector(".bullets-container");
 
   var images = options.images || [];
+  var colorBullet = options.colorBullet || 'red';
+  var autoPlayInterval = options.autoPlayInterval || 3000;
 
   if (!galleryContainer) {
     console.error('Modal gallery container is not defined.');
@@ -36,14 +39,13 @@ function createModalGallery(options) {
   bulletsContainer.innerHTML = images.map(function (_, index) {
     return '<div class="bullet-button page-' + (index + 1) + ' ' + (index === 0 ? 'active' : '') + '" data-index="' + index + '"></div>';
   }).join("");
-  bulletsContainer.querySelector(".active").style.backgroundColor = options.colorBullet || 'red';
-
+  bulletsContainer.querySelector(".active").style.backgroundColor = colorBullet;
 
   new CardCarousel({
     selector: ".gallery-carousel",
     cardsPerSlide: { sm: 1, md: 1, lg: 1 },
     autoPlay: true,
-    autoPlayInterval: 3000,
+    autoPlayInterval: autoPlayInterval,
     onSlideChange: function (currentSlide, slideSize) {
       const bullets = bulletsContainer.querySelectorAll('.bullet-button');
       bullets.forEach(function (bullet) {
@@ -54,7 +56,7 @@ function createModalGallery(options) {
       const activeBullet = bulletsContainer.querySelector(`.bullet-button.page-${currentSlide + 1}`);
       if (activeBullet) {
         activeBullet.classList.add('active');
-        activeBullet.style.backgroundColor = options.colorBullet || 'red';
+        activeBullet.style.backgroundColor = colorBullet;
       };
     },
     onLoaded: function (goToPage) {
@@ -100,9 +102,16 @@ function createModalGallery(options) {
     var bulletsContainer = document.createElement('div');
     bulletsContainer.className = 'bullets-container';
     container.appendChild(bulletsContainer);
-
-
+    
     document.body.appendChild(container);
+
+    // Evento para cerrar el modal con la tecla ESC
+    document.addEventListener('keydown', function escListener(e) {
+      if (e.key === 'Escape') {
+        container.remove();
+        document.removeEventListener('keydown', escListener);
+      }
+    });
 
     // Retorna el contenedor para uso posterior
     return container;
